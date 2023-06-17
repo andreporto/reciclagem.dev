3f4197ea2b8e71bddffb5949f7e90bc43144cca3
André Porto <andreporto@yahoo.com>
2023-06-17 16:34:32 +0000

    Fix initial message

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 function Home() {
-    return `
-        <h1>reciclagem.dev</h1>
-        <h3>* Projetinho usado em mais uma reciclagem de conhecimento na área de desenvolvimento.</h3>`
+    return (
+        <div>
+            <h1>reciclagem.dev</h1>
+            <h3>* Projetinho usado em mais uma reciclagem de conhecimento na área de desenvolvimento.</h3>   
+        </div>
+    )
 }
 
 export default Home;
