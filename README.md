c1d18cc2f6bd6d68f6eb9fc2972a819b16326903
André Porto <andre.porto@serpro.gov.br>
2023-06-27 19:12:19 -0400

    Fix lint script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # reciclagem.dev
+
 Repo para projeto e apontamentos de mais uma reciclagem de conhecimento na área de desenvolvimento.
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 function Home() {
-    return (
-        <div>
-            <h1>reciclagem.dev</h1>
-            <h3>* Projetinho usado em mais uma reciclagem de conhecimento na área de desenvolvimento.</h3>   
-        </div>
-    )
+  return (
+    <div>
+      <h1>reciclagem.dev</h1>
+      <h3>
+        * Projetinho usado em mais uma reciclagem de conhecimento na área de
+        desenvolvimento.
+      </h3>
+    </div>
+  );
 }
 
 export default Home;
